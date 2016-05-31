bb34bee6f68e8e370c39a9188f94f8fffcaa1c3f
Zeljko Tadic <zeljko.tadic.91@gmail.com>
2016-05-31 20:43:45 +0200

    add api caller

--- shifty/webapp/js/utils/api.js
+++ shifty/webapp/js/utils/api.js
@@ -2,7 +2,7 @@ const join = require('urljoin')
 const API_ROOT = 'http://hotels.nextuser.com/api/v1/'
 const QS = ''
 
-function get(url, queryString = QS, data) {
+export function get(url, queryString = QS, data) {
   return new Promise((resolve, reject) => {
     $.get({ url: join(API_ROOT + url, queryString), data })
       .done(response => resolve(response))
@@ -10,7 +10,7 @@ function get(url, queryString = QS, data) {
   })
 }
 
-function post(url, queryString = QS, data) {
+export function post(url, queryString = QS, data) {
   return new Promise((resolve, reject) => {
     $.post({ url: join(API_ROOT + url, queryString), data })
       .done(response => resolve(response))
