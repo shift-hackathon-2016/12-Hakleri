e5d5f056fb2c1271de979a8d371f7e1f18fc0548
Dejan Strbad <dejan.strbad@gmail.com>
2016-05-31 10:55:05 +0200

    modify wsgi

--- shifty/shifty/wsgi.py
+++ shifty/shifty/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for shifty project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shifty.settings")
 
-application = get_wsgi_application()
+if settings.RUNNING_LOCALLY:
+        application = Cling(get_wsgi_application)
+    else:
+        application = get_wsgi_application()
