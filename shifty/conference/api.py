97dcfbea0a72c4aa84f20d1830370b4b3331019e
Marin Bek <marin.bek@gmail.com>
2016-06-01 01:44:07 +0200

    full data

--- shifty/conference/api.py
+++ shifty/conference/api.py
@@ -2,22 +2,17 @@ from tastypie.resources import BaseModelResource, ModelResource, Resource
 from models import *
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization, ReadOnlyAuthorization
+from tastypie import fields
 
 
 class ConferenceResource(ModelResource):
+    company = fields.ForeignKey('shifty.resources.CompanyResource', attribute='company', full=True, null=True, blank=True)
+
     class Meta:
         queryset = Conference.objects.all()
         authentication = Authentication()
         authorization = Authorization()
         resource_name = "conference"
-        always_return_data = True
-
-class ConferenceEventResource(ModelResource):
-    class Meta:
-        queryset = ConferenceEvent.objects.all()
-        authentication = Authentication()
-        authorization = Authorization()
-        resource_name = "conference_event"
         always_return_data = True
 
 class EventTypeResource(ModelResource):
@@ -28,7 +23,21 @@ class EventTypeResource(ModelResource):
         resource_name = "event_type"
         always_return_data = True
 
+class ConferenceEventResource(ModelResource):
+    conference = fields.ForeignKey(ConferenceResource, attribute='conference', full=True, null=True, blank=True)
+    event_type = fields.ForeignKey(EventTypeResource, attribute='event_type', full=True, null=True, blank=True)
+
+    class Meta:
+        queryset = ConferenceEvent.objects.all()
+        authentication = Authentication()
+        authorization = Authorization()
+        resource_name = "conference_event"
+        always_return_data = True
+
 class EventLikeResource(ModelResource):
+    user = fields.ForeignKey('shifty.resources.UserResource', attribute='user', full=True, null=True, blank=True)
+    conference_event = fields.ForeignKey(ConferenceEventResource, attribute='conference_event', full=True, null=True, blank=True)
+
     class Meta:
         queryset = EventLike.objects.all()
         authentication = Authentication()
@@ -37,6 +46,9 @@ class EventLikeResource(ModelResource):
         always_return_data = True
 
 class ConferenceAttendeeResource(ModelResource):
+    conference = fields.ForeignKey(ConferenceResource, attribute='conference', full=True, null=True, blank=True)
+    user = fields.ForeignKey('shifty.resources.UserResource', attribute='user', full=True, null=True, blank=True)
+
     class Meta:
         queryset = ConferenceAttendee.objects.all()
         authentication = Authentication()
@@ -45,6 +57,9 @@ class ConferenceAttendeeResource(ModelResource):
         always_return_data = True
 
 class EventTeamResource(ModelResource):
+    conference_event = fields.ForeignKey(ConferenceEventResource, attribute='conference_event', full=True, null=True, blank=True)
+    team = fields.ForeignKey('shifty.resources.TeamResource', attribute='team', full=True, null=True, blank=True)
+
     class Meta:
         queryset = EventTeam.objects.all()
         authentication = Authentication()
@@ -53,6 +68,10 @@ class EventTeamResource(ModelResource):
         always_return_data = True
 
 class EventJudgeVoteResource(ModelResource):
+    judge = fields.ForeignKey('shifty.resources.UserResource', attribute='judge', full=True, null=True, blank=True)
+    team = fields.ForeignKey('shifty.resources.TeamResource', attribute='team', full=True, null=True, blank=True)
+    conference_event = fields.ForeignKey(ConferenceEventResource, attribute='conference_event', full=True, null=True, blank=True)
+
     class Meta:
         queryset = EventJudgeVote.objects.all()
         authentication = Authentication()
@@ -61,12 +80,12 @@ class EventJudgeVoteResource(ModelResource):
         always_return_data = True
 
 class EventCommentResource(ModelResource):
+    user = fields.ForeignKey('shifty.resources.UserResource', attribute='user', full=True, null=True, blank=True)
+    conference_event = fields.ForeignKey(ConferenceEventResource, attribute='conference_event', full=True, null=True, blank=True)
+
     class Meta:
         queryset = EventComment.objects.all()
         authentication = Authentication()
         authorization = Authorization()
         resource_name = "event_comment"
         always_return_data = True
-
-
-
--- shifty/shifty/resources.py
+++ shifty/shifty/resources.py
@@ -3,6 +3,7 @@ from social.apps.django_app.utils import load_strategy, load_backend
 from models import *
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization, ReadOnlyAuthorization
+from tastypie import fields
 
 from authentication import SocialAuthentication
 from social.apps.django_app.views import NAMESPACE
@@ -10,17 +11,9 @@ from social.apps.django_app.views import NAMESPACE
 from django.core.urlresolvers import reverse
 
 
-class UserResource(ModelResource):
-    class Meta:
-        queryset = ShiftUser.objects.all()
-        excludes = ['email', 'password', 'is_superuser', 'is_staff']
-        allowed_methods = ['get']
-        authentication = Authentication()
-        authorization = ReadOnlyAuthorization()
-        resource_name = "user"
-        always_return_data = True
 
 class TeamResource(ModelResource):
+
     class Meta:
         queryset = Team.objects.all()
         authentication = Authentication()
@@ -34,4 +27,17 @@ class CompanyResource(ModelResource):
         authentication = Authentication()
         authorization = Authorization()
         resource_name = "company"
+        always_return_data = True
+
+class UserResource(ModelResource):
+    team = fields.ForeignKey(TeamResource, attribute='team', full=True, null=True, blank=True)
+    company = fields.ForeignKey(CompanyResource, attribute='company', full=True, null=True, blank=True)
+
+    class Meta:
+        queryset = ShiftUser.objects.all()
+        excludes = ['email', 'password', 'is_superuser', 'is_staff']
+        allowed_methods = ['get']
+        authentication = Authentication()
+        authorization = ReadOnlyAuthorization()
+        resource_name = "user"
         always_return_data = True
