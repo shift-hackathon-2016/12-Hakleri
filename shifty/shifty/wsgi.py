3be05804c8a4837a1d8a30d5e1e7d451939a064b
Dejan Strbad <dejan.strbad@gmail.com>
2016-05-31 11:18:37 +0200

    fix wsgi

--- shifty/shifty/wsgi.py
+++ shifty/shifty/wsgi.py
@@ -7,6 +7,6 @@ from django.conf import settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shifty.settings")
 
 if settings.RUNNING_LOCALLY:
-    application = Cling(get_wsgi_application)
+    application = Cling(get_wsgi_application())
 else:
     application = get_wsgi_application()
